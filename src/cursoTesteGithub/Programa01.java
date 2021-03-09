4ce9b5c650b698569070de80ec17cc0be7065cee
Ralph Aguiar <ralphaguiar@gmail.com>
2021-03-09 19:20:35 -0300

    versao 002

--- src/cursoTesteGithub/Programa01.java
+++ src/cursoTesteGithub/Programa01.java
@@ -4,6 +4,9 @@ public class Programa01 {
 
 	public static void main(String[] args) {
 		System.out.println("OKKKK!!");
+		System.out.println("OKKKK!!");
+		System.out.println("OKKKK!!");
+		System.out.println("OKKKK!!");
 	}
 
 }
